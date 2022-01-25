ce5259bd80f13ae7382557b437c75edaed951cc2
Daniel Zoba <daniel.sobe@sorben.com>
2022-01-25 14:48:30 +0100

    simplify lexicon generation

--- examples/ex3/input/runme.sh
+++ examples/ex3/input/runme.sh
@@ -4,7 +4,6 @@ cd "${0%/*}"
 
 python3 BASgenerator.py HSB.yaml
 
-mv corpus/ sentences/ speechrecorder/ transliterations/ /results/
+rm -f /results/*
+cp corpus/hsb_sampa.lex /results/
 
-echo "Please add the file 'SpeechRecPrompts_4.dtd' manually to every Speechrecorder project!"
-echo "Either take the one that is checked in, or let Speechrecorder create a new one when creating a new project."
